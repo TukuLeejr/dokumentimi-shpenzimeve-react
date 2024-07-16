dcb045effe4aaeeee794d2a2195d3c9c6e394d99
TukuLee <121036323+TukuLeejr@users.noreply.github.com>
2024-07-16 20:49:56 +0200

    Finshing up the components

---
+++ src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// A reducer is used to specify the application state changes in response to certain actions
+
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
